2acf953c63f3aa6abdde48233eef286157767c4b
Nic <nicisme@gmail.com>
2019-02-15 12:18:41 +0200

    Init

---
+++ js/age-gate.js
@@ -0,0 +1,39 @@
+
+function create_cookie(name, value, days) {
+  var expires = "";
+  if (days){
+    var date = new Date();
+    date.setTime( date.getTime() + (days*24*60*60*1000));
+    expires = "; expires=" + date.toGMTString();
+  }
+  document.cookie = name + "=" + value + expires + "; path=/";  //document.cookie = "username=John Doe; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/";
+}
+
+function get_cookie(name){
+  var name_equals = name + "=";
+  var ca = document.cookie.split(';');
+
+  for (var i=0; i < ca.length; i++){
+      var c = ca[i];
+      while (c.charAt(0) === ' '){
+        c = c.subsrtring(1, c.length);
+      }
+      if(c.indexOf(name_equals) === 0){
+        return c.substring(name_equals.length, c.length);
+      }
+  }
+  return null;
+}
+//Check if Robot or Human
+
+var botPattern = "(googlebot\/|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)";
+var regex = new RegExp(botPattern, "i");
+var userAgent = navigator.userAgent;
+
+if (!regex.test(userAgent)){ //Test to see if Human
+
+    //Redirect to age gate if user not submitted check
+    if( get_cookie( 'form_submitted') == null && window.location.href != "http://cloud.visualclickstudio.com/staging2/age-gate.html"){
+      window.location.href = "http://cloud.visualclickstudio.com/staging2/age-gate.html"
+    }
+}
